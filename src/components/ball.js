62d066443eb970947686cc8b1d39bb58b9a91592
Anwar Magara <sadatt254@gmail.com>
2019-03-07 18:18:05 +0300

    added the bounce effect

---
+++ src/components/ball.js
@@ -0,0 +1,27 @@
+class Ball{
+	constructor(elem, elemH, x, y, dx, dy, radious){
+		this.elemH = elemH;
+		this.elem = elem;
+		this.x = x;
+		this.y = y;
+		this.dx = dx;
+		this.dy = dy;
+		this.radious = radious;
+	}
+
+	draw(){
+		this.elem.beginPath();
+		this.elem.arc(this.x , this.y, this.radious, 0, 2 * Math.PI);
+		this.elem.fillStyle = "#ff5e3a";
+		this.elem.fill()
+	}
+
+	update(){
+		if( this.y<0 || this.y>this.elemH.height) this.dy=-this.dy;
+		this.y += this.dy;
+		this.draw()
+		console.log(this.radious)
+	}
+}
+
+export default Ball;
--- src/components/loader.js
+++ src/components/loader.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from 'react';
+import Ball from './ball'
 
 export default class Loader extends React.Component{
+	componentDidMount(){
+		const elem = document.getElementById('canvas');
+		let ctx = elem.getContext('2d')
+		const ball = new Ball(ctx, elem, 100, 20, 3, 3, 10);
+
+		function animate(){
+			requestAnimationFrame(animate);
+			ctx.clearRect(0,0, elem.width, elem.height);
+			ball.update();
+		}
+		animate();
+	}
     render(){
         return(
             <div className="tsk-loader">
                 <div className="loader-container">
-                    <div className="bounce" id="loader"></div>
+                	<canvas id="canvas"/>
                 </div>
             </div>
         )
